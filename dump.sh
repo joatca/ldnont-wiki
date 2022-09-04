cf4dfdf02b5f334496827d42f0fb744445c026b3
Wiki Owner <ldnont@wiki>
2022-09-04 20:32:39 +0000

    automatic dump at Sun Sep  4 20:32:39 UTC 2022

--- dump.sh
+++ dump.sh
@@ -4,3 +4,7 @@ cd /usr/share/webapps/mediawiki
 php7 ./maintenance/dumpBackup.php --full | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-full.xml.xz
 php7 ./maintenance/dumpBackup.php --current | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-current.xml
 sed -e 's/^\$wgSecretKey.*$/$wgSecretKey = "**CENSORED**";/' -e 's/^\$wgUpgradeKey.*$/$wgUpgradeKey = "**CENSORED**";/' LocalSettings.php >/usr/share/webapps/git/ldnont/LocalSettings.php
+cd /usr/share/webapps/git/ldnont
+git add .
+git commit -m "automatic dump at $(date)"
+git push
