4f75690660e60b6eb2f2ed772c4118dc33412203
Wiki Owner <ldnont@wiki>
2022-09-04 20:29:07 +0000

    fix dump script to censor sensitive values in LocalSettings.php

--- dump.sh
+++ dump.sh
@@ -3,4 +3,4 @@
 cd /usr/share/webapps/mediawiki
 php7 ./maintenance/dumpBackup.php --full | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-full.xml.xz
 php7 ./maintenance/dumpBackup.php --current | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-current.xml
-cp LocalSettings.php /usr/share/webapps/git/ldnont
+sed -e 's/^\$wgSecretKey.*$/$wgSecretKey = "**CENSORED**";/' -e 's/^\$wgUpgradeKey.*$/$wgUpgradeKey = "**CENSORED**";/' LocalSettings.php >/usr/share/webapps/git/ldnont/LocalSettings.php
