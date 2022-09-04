9d5834c888eda40cc6b3bb334e9cd8364e4cb2c6
Wiki Owner <ldnont@wiki>
2022-09-04 20:36:47 +0000

    don't dump current as compressed

--- dump.sh
+++ dump.sh
@@ -2,7 +2,7 @@
 
 cd /usr/share/webapps/mediawiki
 php7 ./maintenance/dumpBackup.php --full | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-full.xml.xz
-php7 ./maintenance/dumpBackup.php --current | pixz -9 >/usr/share/webapps/git/ldnont/ldnont-current.xml
+php7 ./maintenance/dumpBackup.php --current >/usr/share/webapps/git/ldnont/ldnont-current.xml
 sed -e 's/^\$wgSecretKey.*$/$wgSecretKey = "**CENSORED**";/' -e 's/^\$wgUpgradeKey.*$/$wgUpgradeKey = "**CENSORED**";/' LocalSettings.php >/usr/share/webapps/git/ldnont/LocalSettings.php
 cd /usr/share/webapps/git/ldnont
 git add .
